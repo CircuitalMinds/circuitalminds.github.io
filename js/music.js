95d5b2258005e1a83b57bdedef10fd64344784d9
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-03-15 04:00:42 -0700

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,13 +1,3 @@
-var playList;
-var musicApp;
-$( function () {
-      var getData = $.get("https://circuitalminds.herokuapp.com/get_template/music");
-      getData.done( function( data ) {
-          playList = data['playlist'];
-          musicApp = data['music_app'];
-      });
-});
-
 function shareVideo() {
       var el = document.createElement('textarea');
       songData = document.getElementById("play-media").src.split("/");
