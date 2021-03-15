7cd21bfc13c3f7479ee0cad6d7a83d7dde1df530
Alan Matzumiya <alan.matzumiya@outlook.es>
2021-03-15 03:51:11 -0700

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,66 +1,22 @@
-var currentSong = document.getElementById('play-song');
-var playMedia = document.getElementById('play-media');
-var searchList = document.getElementById("search-list");
+var playList;
+var musicApp;
+$( function () {
+      var getData = $.get("https://circuitalminds.herokuapp.com/get_template/music");
+      getData.done( function( data ) {
+          playList = data['playlist'];
+          musicApp = data['music_app'];
+      });
+});
 
-function endSong() {
-    indexSong = parseInt(indexSong) + 1;
-    getSong( indexSong );
-};
-
-function songFromList(id) {
-    indexSong = parseInt(id);
-    getSong( indexSong );
-};
-
-function getSong(song_id) {
-    var getData = $.get("/music/play", {"song_id": song_id});
-    getData.done( function( data ) {
-       playMedia.setAttribute("src", data['url']);
-       currentSong.innerHTML = data['name'];
-       playMedia.play();
-    });
-};
-
-function youtubeSearch(search) {
-    waitLabel = document.createElement("li");
-    waitLabel.innerHTML = '<div data-role="progress" data-type="line"></div>';
-    searchList.appendChild(waitLabel);
-    var searchData = $.get("/youtube/search/" + search);
-    searchData.done( function( data ) {
-       var dataList = data['search_data'];
-       var lenData = dataList.length;
-       searchList.innerHTML = "";
-
-       for (var j = 0; j < lenData; j++) {
-           row = document.createElement("li");
-           rowContent = document.createElement("ul");
-           rowContent.setAttribute("class", "group-list horizontal text-center");
-
-           element1 = document.createElement("li");
-           element1.textContent= dataList[j]['video_title'];
-           rowContent.appendChild(element1);
-
-           element2 = document.createElement("li");
-           url = document.createElement("a");
-           url.setAttribute("class", "text-center fg-teal")
-           url.setAttribute("href", dataList[j]["video_url"]);
-           url.textContent = dataList[j]["video_url"];
-           element2.appendChild(url);
-           rowContent.appendChild(element2);
-
-           element3 = document.createElement("li");
-           element3.textContent = "Get Song";
-           element3.setAttribute("class", "button primary text-center fg-teal fg-light-hover bg-light bg-teal-hover");
-           element3.setAttribute("onClick", "youtubeDownloader('" + dataList[j]['video_url'] + "')");
-           rowContent.appendChild(element3);
-
-           row.appendChild(rowContent);
-           searchList.appendChild(row);
-       }
-
-    });
-};
-
-function youtubeDownloader(url) {
-    var urlData = $.get("/check_data/select", {"url": url});
-};
+function shareVideo() {
+      var el = document.createElement('textarea');
+      songData = document.getElementById("play-media").src.split("/");
+      songName = songData[songData.length - 1].replace(".mp4?raw=true", "");
+      shareURL = "https://circuitalminds.herokuapp.com/music?play_song=" + encodeURI(songName);
+      el.value = shareURL;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      alert("Link Copied: " + el.value);
+      document.body.removeChild(el);
+}
