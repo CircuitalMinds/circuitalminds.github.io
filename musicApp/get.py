2af3a4d65444bbf4cb89865af809d090e1c58ca0
alanmatzumiya <alan.matzumiya@outlook.es>
2021-03-31 11:49:26 -0700

    auto

--- musicApp/get.py
+++ musicApp/get.py
@@ -5,15 +5,25 @@ import requests
 
 json_files = {"table": "table_data.json", "music_data": "music_data.json"}
 
+header = [{"name": "id", "title": "ID", "size": 50, "sortable": "true", "sortDir": "asc", "format": "number"}, 
+          {"name": "name", "title": "Name", "sortable": "true"}]
+
 def save_data(data):
-    with open("music_data.json", "w") as outfile:
+    with open("table_data.json", "w") as outfile:
         json_file = json.dumps(data, indent=4, sort_keys=False)
         outfile.write(json_file)
         outfile.close()
 
 def get_table_data():
-    json_data = json.load(open(json_files["table"]))
-    print(json_data)
+    music_data = json.load(open(json_files["music_data"]))
+    json_data = {'header': header}
+    data = []    
+    s = 1
+    for song in music_data['data']:
+        data.append([s, f'<button style="border: 1px solid dark; margin: 0; width: 100%; height: 100%;" class="button fg-teal" onclick="songFromList({s});">{song["video_title"]}</button>'])
+        s += 1
+    json_data['data'] = data
+    save_data(json_data)
     return json_data
 
 def get_music_data():
@@ -24,4 +34,4 @@ def get_music_data():
     json_data['data'] = data
     save_data(json_data)
     
-get_music_data()
+get_table_data()
