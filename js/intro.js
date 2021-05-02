9529c483111df4a3f55b879f05b34f1f9d4437b8
alanmatzumiya <alanmatzumiya@outlook.es>
2021-05-01 17:06:23 -0700

    auto

--- js/intro.js
+++ js/intro.js
@@ -6,3 +6,18 @@ function stop () {
   document.getElementById("text-init").innerHTML = "";
   document.getElementById("mybtn").innerHTML = "<button onclick='init()'><h4>Click Me!</h4></button>";
   }
+
+$( function () {
+    var index = 0;
+    let videoLocation = circuitApi.url_content + "circuitalminds.github.io/main/musicApp/music_data_list.json";
+    let videoRequest = new XMLHttpRequest();
+    videoRequest.open('GET', videoLocation);
+    videoRequest.responseType = 'json';
+    videoRequest.send();
+    videoRequest.onload = function() {
+        const videoData = videoRequest.response;
+        circuitApi.music_app.song_list = videoData['music_data_list'];
+        index = Math.round(Math.random() * (videoData['music_data_list'].length - 1));
+        setPlayer(index);
+    };
+});
